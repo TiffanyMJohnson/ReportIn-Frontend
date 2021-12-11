a7d44443ef6e57f9b6c0df44d2784ac5a0dcc8e4
tiffanyjohnson <johnson.tiffany0585@gmail.com>
2021-12-11 10:36:29 -0800

    save 3

--- reportin/src/App.js
+++ reportin/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/imports/Header'
+import Footer from './components/imports/Footer'
+import Landing from './components/pages/Landing'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Memo from './components/pages/Memo'
+
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+
+let baseUrl = process.env.REACT_APP_BASEURL
+
+class App extends Component {
+
+  render () {
+    return (
+          <div className="App">
+            <BrowserRouter>
+              <Header />
+                <Switch>
+                  <Route exact path='/' component={Landing}></Route>
+                  {/* <Route path='/login' component={Login}></Route>
+                  <Route path='/register' component={Register}></Route>
+                  <Route path='/memo' component={Memo}></Route> */}
+                  
+                </Switch>
+  
+                <img id="home" src="https://parade.com/wp-content/uploads/2019/04/pet-quotes.jpg" alt=""></img>
+  
+              <Footer />
+            </BrowserRouter>
+         
+            
+          </div>
+      );
+    }
+  }
+
+
 
 export default App;
---
+++ reportin/src/components/pages/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomeBtns from '../imports/HomeBtns'
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+         <div>
+           <HomeBtns />
+           <p> The Gunslingers Drill Team was formed in 1991 with thier Red and Black western-styled uniforms. The team is comprised of members ranging in age from 6 to 19 with differing experience levels. With the complexity of rifles and hand drills in thier military style routines, the Gunslingers travel throughout Washington state participating in different competitions and parades. The Gunslingers Drill team is based out of Lake Stevens, Washington, but their members come from all over the Snohomish County area (Lynnwood, Everett, Lake Stevens, Marysville, and Arlington). If you or your child are intrested in joining or would like additional information please contact us. </p>
+            <img id="teamphoto" src="https://i.imgur.com/cVktUBg.jpg"></img>
+         </div>
+        </main>
+    </div>
+  )
+}
